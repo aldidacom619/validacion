1b30b49d3e74ccf7de10c0303b8bc968dec9610e
aldidacom619 <aldidacom@gmail.com>
2021-03-10 16:20:09 -0400

    mensaje

--- application/controllers/Administrador.php
+++ application/controllers/Administrador.php
@@ -18,7 +18,7 @@ class Administrador extends CI_Controller
         $this->usuario['usuario'] = $this->session->userdata('nombre_completo');
         $this->_is_logued_in();
     }
-    //pruebas ****** ****
+    //pruebas ****** **** ********* *********
     function _is_logued_in(){ 
         $is_logued_in = $this->session->userdata('is_logued_in');        
         $tipo_user = $this->session->userdata('administrador');
